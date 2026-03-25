51bcb3e18d9d77db058bdd12695e976378e9ddb8
izhank216 <izhank216@gmail.com>
2026-03-25 18:48:43 +0500

    Update TextureCorruptionMixin.java

--- src/main/java/com/izhan/texturecorrupter/mixin/TextureCorruptionMixin.java
+++ src/main/java/com/izhan/texturecorrupter/mixin/TextureCorruptionMixin.java
@@ -1,7 +1,10 @@
 package com.izhan.texturecorrupter.mixin;
 
 import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.SpriteContents;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,24 +12,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(NativeImage.class)
+@Mixin(SpriteContents.class)
 public abstract class TextureCorruptionMixin {
-    @Shadow private int width;
-    @Shadow private int height;
+    @Shadow @Final @Mutable private int width;
+    @Shadow @Final @Mutable private int height;
+    @Shadow @Final private NativeImage image;
 
     private final Random random = new Random();
 
-    @Inject(method = "<init>(Lnet/minecraft/client/texture/NativeImage$Format;IIZJ)V", at = @At("RETURN"))
+    @Inject(method = "<init>", at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
-        NativeImage img = (NativeImage)(Object)this;
-        
         if (this.width <= 0 || this.height <= 0) return;
 
         for (int y = 0; y < this.height; y++) {
             for (int x = 0; x < this.width; x++) {
                 if (random.nextFloat() < 0.15f) {
-                    int color = img.getColorArgb(x, y);
-                    img.setColor(x, y, color ^ random.nextInt(0xFFFFFF));
+                    int color = image.getColorArgb(x, y);
+                    image.setColorArgb(x, y, color ^ random.nextInt(0xFFFFFF));
                 }
             }
         }
