32ac31a5c565428789fffcacc5302d4163e7909e
izhank216 <izhank216@gmail.com>
2026-03-25 18:00:23 +0500

    Update TextureCorruptionMixin.java

--- src/main/java/com/izhan/texturecorrupter/mixin/TextureCorruptionMixin.java
+++ src/main/java/com/izhan/texturecorrupter/mixin/TextureCorruptionMixin.java
@@ -5,7 +5,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
@@ -15,30 +14,26 @@ public abstract class TextureCorruptionMixin {
     @Shadow private int width;
     @Shadow private int height;
 
-    private static final Random RND = new Random();
-
-    @ModifyVariable(method = "<init>(Lnet/minecraft/client/texture/NativeImage$Format;IIZJ)V", at = @At("HEAD"), ordinal = 0)
-    private static int modifyWidth(int w) {
-        return w > 0 && RND.nextFloat() < 0.1f ? w + (RND.nextInt(17) - 8) : w;
-    }
-
-    @ModifyVariable(method = "<init>(Lnet/minecraft/client/texture/NativeImage$Format;IIZJ)V", at = @At("HEAD"), ordinal = 1)
-    private static int modifyHeight(int h) {
-        return h > 0 && RND.nextFloat() < 0.1f ? h + (RND.nextInt(17) - 8) : h;
-    }
+    private final Random random = new Random();
 
     @Inject(method = "<init>(Lnet/minecraft/client/texture/NativeImage$Format;IIZJ)V", at = @At("RETURN"))
     private void onInit(CallbackInfo ci) {
         NativeImage img = (NativeImage)(Object)this;
-        int w = this.width;
-        int h = this.height;
-        if (w <= 0 || h <= 0) return;
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                if (RND.nextFloat() < 0.20f) {
-                    img.setColor(x, y, img.getColor(x, y) ^ RND.nextInt(0xFFFFFF));
+        
+        if (this.width <= 0 || this.height <= 0) return;
+
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                if (random.nextFloat() < 0.15f) {
+                    int color = img.getColor(x, y);
+                    img.setColor(x, y, color ^ random.nextInt(0xFFFFFF));
                 }
             }
+        }
+
+        if (random.nextFloat() < 0.05f) {
+            this.width = Math.max(1, this.width + (random.nextInt(11) - 5));
+            this.height = Math.max(1, this.height + (random.nextInt(11) - 5));
         }
     }
 }
