31a51fa43debb282cd05b2c3355349f98cc006ff
izhank216 <izhank216@gmail.com>
2026-08-08 17:01:47 +0500

    lemme fix bug rq

--- src/client/java/com/izhan/texturecorruptor/mixin/client/PanoramaCorruptionMixin.java
+++ src/client/java/com/izhan/texturecorruptor/mixin/client/PanoramaCorruptionMixin.java
@@ -3,22 +3,23 @@ package com.izhan.texturecorruptor.mixin.client;
 
 import net.minecraft.client.renderer.PanoramaRenderer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(PanoramaRenderer.class)
 public class PanoramaCorruptionMixin {
     @ModifyConstant(
-            method = "render(FF)V",
-            constant = @Constant(intValue = 256)
+            method = "render(Lnet/minecraft/client/gui/GuiGraphics;IIZ)V",
+            constant = @Constant(intValue = 16)
     )
     private int corruptPanorama1(int constant) {
         return constant * 3;
     }
 
     @ModifyConstant(
-            method = "render(FF)V",
-            constant = @Constant(intValue = 1024)
+            method = "render(Lnet/minecraft/client/gui/GuiGraphics;IIZ)V",
+            constant = @Constant(intValue = 128)
     )
     private int corruptPanorama2(int constant2) {
         return 20;
