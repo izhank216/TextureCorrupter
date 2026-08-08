5158c71357e0786a011c809b40bdfc78f76b9b72
izhank216 <izhank216@gmail.com>
2026-08-08 17:30:41 +0500

    Update PanoramaCorruptionMixin.java

--- src/client/java/com/izhan/texturecorruptor/mixin/client/PanoramaCorruptionMixin.java
+++ src/client/java/com/izhan/texturecorruptor/mixin/client/PanoramaCorruptionMixin.java
@@ -1,26 +1,18 @@
 // this was edited from logo corruption mixin
 package com.izhan.texturecorruptor.mixin.client;
 
-import net.minecraft.client.renderer.CubeMap;
+import net.minecraft.client.renderer.PanoramaRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(CubeMap.class)
+@Mixin(PanoramaRenderer.class)
 public class PanoramaCorruptionMixin {
     @ModifyConstant(
             method = "render",
-            constant = @Constant(floatValue = 85.0F)
+            constant = @Constant(floatValue = 0.1F)
     )
-    private float corruptPanoramaFOV(float constant) {
-        return 360.0F;
-    }
-
-    @ModifyConstant(
-            method = "render",
-            constant = @Constant(floatValue = 10.0F)
-    )
-    private float corruptPanoramaDistance(float constant) {
-        return -50.0F;
+    private float corruptPanoramaSpin(float constant) {
+        return 50.0F;
     }
 }
